78c847915b5ca3b5cde898761b696f169aa82e62
Mrinalummadising <mrinalummadising.com>
2023-09-14 14:00:16 +0000

    Conditional Rendering in ReactJS

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import {Component} from 'react'
+
+import Welcome from './components/Welcome'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    isLoggedIn: true,
+  }
+
+  render() {
+    const {isLoggedIn} = this.state
+    let authButton
+    if (isLoggedIn === true) {
+      authButton = <button type="button">Logout</button>
+    } else {
+      authButton = <button type="button">Login</button>
+    }
+
+    return (
+      <div className="container">
+        <Welcome greeting="Hello" name="User" />
+        {authButton}
+      </div>
+    )
+  }
+}
+
+export default App
